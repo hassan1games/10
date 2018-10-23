2be0d2000eebcd6c93d626622ec3204c61dee695
hassangamesdiscord <44375561+hassangamesdiscord@users.noreply.github.com>
2018-10-23 23:10:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,6 +19,23 @@ client.user.setStatus("dnd")
 
 
 
+client.on('message', function(message) {
+    if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        var stewart = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTimestamp()
+            .setTitle('``رساله جديده في خاص البوت``')
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
+            .setFooter(`من (@${message.author.tag})  |  (${message.author.id})`)
+        client.channels.get("503226588763127828").send({ embed: stewart });
+    }
+});
+
+
+
+
 client.on('message', message => {
     if (message.content.startsWith("%رابط")) {
  
