fa181df78441c0c348e7fd5ff71159abe92c5749
hassangamesdiscord <44375561+hassangamesdiscord@users.noreply.github.com>
2018-10-24 18:27:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,6 +34,21 @@ client.on('message', function(message) {
 });
 
 
+client.on("message", message => {
+    var prefix = "%"
+    if (!message.content.startsWith(prefix)) return;
+      let command = message.content.split(" ")[0];
+      command = command.slice(prefix.length);
+        if(command === "skin") {
+                const args = message.content.split(" ").slice(1).join(" ")
+        if (!args) return message.channel.send("** Type your skin name **");
+        const image = new Discord.Attachment(`https://visage.surgeplay.com/full/256/${args}`, "skin.png");
+    message.channel.send(image)
+        }
+    });
+
+
+
 
 client.on('guildMemberAdd', Sal => { //By Salto7#4595
     var embed = new Discord.RichEmbed()
