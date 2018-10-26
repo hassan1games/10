528c7195e0514e2c6aeaba782c53d8f7c57dfe15
hassangamesdiscord <44375561+hassangamesdiscord@users.noreply.github.com>
2018-10-26 07:36:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ client.on("ready", () => {
 
 
 
-
+const fs = require('fs');
 let profile = JSON.parse(fs.readFileSync("./profile.json", "utf8"))
 client.on("message", message => {
   if (message.author.bot) return;
